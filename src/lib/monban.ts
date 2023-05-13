33896ec61115ce17a154721530a73b2f9a468634
kosei28 <niwa.kosei@gmail.com>
2023-05-14 01:30:24 +0900

    update

--- src/lib/monban.ts
+++ src/lib/monban.ts
@@ -1,56 +1,39 @@
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, JWT_SECRET } from '$env/static/private';
-import { Monban, MemorySessionStore, UserManager } from 'monban';
-import { GoogleProvider, type InferAccountInfo } from 'monban/providers';
+import { Monban, type Session } from 'monban';
+import { GoogleProvider } from 'monban/providers/google/server';
 
-type User = {
+export type SessionUser = {
     id: string;
     name: string;
     email: string;
-    picture: string;
-    provider: string;
 };
 
-const providers = {
-    google: new GoogleProvider({
-        clientId: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET
-    })
-};
+export const monban = new Monban(
+    {
+        google: new GoogleProvider({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        })
+    },
+    {
+        secret: JWT_SECRET,
+        callback: {
+            async createSession(accountInfo, userId) {
+                const session: Session<SessionUser> = {
+                    id: undefined,
+                    user: {
+                        id: userId,
+                        name: accountInfo.name,
+                        email: accountInfo.email
+                    }
+                };
 
-type AccountInfo = InferAccountInfo<typeof providers>;
+                return session;
+            },
 
-declare global {
-    // eslint-disable-next-line no-var
-    var users: Record<string, User> | undefined;
-}
-
-class MyUserManager extends UserManager<AccountInfo> {
-    async createUser(accountInfo: AccountInfo): Promise<string> {
-        if (globalThis.users === undefined) {
-            globalThis.users = {};
-        }
-
-        globalThis.users[accountInfo.id] = accountInfo;
-
-        return accountInfo.id;
-    }
-
-    async getUser(userId: string): Promise<User | undefined> {
-        if (globalThis.users === undefined) {
-            return undefined;
-        }
-
-        return globalThis.users[userId];
-    }
-
-    async deleteUser(userId: string): Promise<void> {
-        if (globalThis.users !== undefined) {
-            delete globalThis.users[userId];
+            async refreshSession(session) {
+                return session;
+            }
         }
     }
-}
-
-const sessionStore = new MemorySessionStore();
-const userManager = new MyUserManager();
-
-export const monban = new Monban(providers, sessionStore, userManager, { secret: JWT_SECRET });
+);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,35 @@
 <script lang="ts">
     import { browser } from '$app/environment';
+    import type { SessionUser } from '$lib/monban';
+    import type { Session } from 'monban';
+    import { googleSignIn } from 'monban/providers/google/client';
 
-    type User = {
-        id: string;
-        name: string;
-        email: string;
-        picture: string;
-        provider: string;
-    };
-
-    let user: User | undefined = undefined;
+    let session: Session<SessionUser> | undefined = undefined;
 
     if (browser) {
-        getUser();
+        getSession();
     }
 
-    async function getUser() {
-        const res = await fetch('/monban/me');
-        user = await res.json();
+    async function getSession() {
+        const res = await fetch('/monban/me/session');
+        session = await res.json();
     }
 
-    async function logout() {
-        await fetch('/monban/logout');
-        user = undefined;
+    async function signIn() {
+        await googleSignIn('/monban');
+    }
+
+    async function signOut() {
+        await fetch('/monban/signout');
+        session = undefined;
     }
 </script>
 
 <h1>SvelteKit Google Auth</h1>
 
-{#if user === undefined}
-    <a href="/monban/login/google">Login with Google</a>
+{#if session === undefined}
+    <button on:click={signIn}>Login with Google</button>
 {:else}
-    <p>{user.name}</p>
-    <button on:click={logout}>Logout</button>
+    <p>{session.user.name}</p>
+    <button on:click={signOut}>Logout</button>
 {/if}
